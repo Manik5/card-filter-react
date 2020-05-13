91fd0ed9520246b2bf9fed3828e86e1e9f47caf5
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-13 19:15:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SearchBox from './components/SearchBox';
+
+class App extends React.Component{
+  render() {
+    return(
+      <div className="MainBox">
+          <h2 className="text">Search for a card</h2>
+        <div className="App">
+        </div>
+         <div>
+           <SearchBox items/>
+         </div>
+      </div>
+    )
+  }
+};
 
 export default App;
+
---
+++ src/components/SearchBox.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import './SearchBox.css';
+
+function SearchBox() {
+  const [sourceData, setSourceData] = useState([]);
+  let [filteredData, setFilteredData] = useState([]);
+  let [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.scryfall.com/catalog/card-names')
+      .then((response) => response.json())
+      .then((cards) => {
+        const data = cards.data.slice(10, 15000);
+        setSourceData(data);
+        setFilteredData(data);
+      });
+  }, []);
+
+  const onTextChange = (event) => {
+    setSearchText(event.target.value);
+    const regex = new RegExp(searchText, 'i');
+    let data = sourceData.filter((word) => regex.test(word));
+    setFilteredData(data);
+  };
+
+  const onItemClick = (value) => {
+    setSearchText(value);
+    setFilteredData([]);
+  };
+
+  const renderedFilterData = () => {
+    if (!filteredData) return null;
+
+    return (
+      <ul style={{listStyle:"none"}}>
+        {filteredData.map((item, index) => (
+          <li key={index} onClick={() => onItemClick(item)}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  const onClearClick = () => {
+    setSearchText('');
+    setFilteredData(sourceData);
+  };
+
+
+
+  return (
+    <div className="BoxInput">
+      <br />
+     <div>
+        <input className="box" value={searchText} onChange={onTextChange} type="text" placeholder="Search..." />
+        <button className="clear" onClick={onClearClick}>Clear</button>
+        {renderedFilterData()}
+     </div>
+    </div>
+  );
+}
+
+export default SearchBox;
